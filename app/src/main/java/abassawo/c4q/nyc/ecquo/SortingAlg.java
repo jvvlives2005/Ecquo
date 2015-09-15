6d671c278de97114419c3d9561772fe138874382
personabe1984 <personabe1984@gmail.com>
2015-09-15 17:37:33 -0400

    added the outline of the sorting

--- app/src/main/java/abassawo/c4q/nyc/ecquo/SortingAlg.java
+++ app/src/main/java/abassawo/c4q/nyc/ecquo/SortingAlg.java
@@ -1,51 +1,70 @@
 package abassawo.c4q.nyc.ecquo;
 
+
+/**
+ * Rating algorithm
+ *   1.closes due date
+ *   2.priority
+ *   3.location
+ *
+ *   User
+ *      1.Current Location
+ *      2.Current day
+ *
+ *   Weighting sort
+ *   (0.5) (Date Score)
+ *   (0.4) (Priority Score)
+ *   (0.1) (Location Score)
+ */
+
 public class SortingAlg {
 
-    private double mCurrentTime, mDueTime, mLocation;
+    private double mTime, mLocation, m;
     private String mDate;
+    private int mGreatest, mPriority;
 
 
     // Need a method to sort out the days
 
-    public void sortingByTheDays{
+    public SortingAlg(){
 
-        switch (mDate){
+    }
 
-            case
-                //Monday
-
-            case
-                //Tuesday
-
-            case
-                //Wednesday
-
-            case
-                //Thursday
-
-            case
-                //Friday
-
-            case
-                //Saturday
-
-            case
-                //Sunday
-
-            default
+    public void sorting(int[] array){
+        for(int i = 1; i < array.length; i++){
+            mGreatest = 0;
+            if(mGreatest < array[i]){
+                mGreatest = array[i];
+            }
         }
+    }
 
+    public double rankingLocation(double currentLocation , double setPlace){
+        double rank;
+        rank = (0.1) * (setPlace - currentLocation);
+        return rank;
+    }
+
+    public double rankingTime(double currentTime, double dueTime){
+        double rank;
+        rank = (0.5) * (dueTime - currentTime);
+        return rank;
+    }
+
+    public double rankingPriority(int priority){
+        double rank;
+        rank = (0.4) * priority;
+        return rank;
+    }
+
+    public void convertLocationIntoMeter(double lan, double lag){
 
     }
 
-    public void sortingByTime(){
-
+    public void getGreatet(){
+        this.mGreatest = mGreatest;
     }
 
-    public void sortingByGeoLocation(){
-
-    }
 
 
 }
---
+++ app/src/main/java/abassawo/c4q/nyc/ecquo/user/db/NavigationDrawerItem.java
@@ -0,0 +1,20 @@
+package abassawo.c4q.nyc.ecquo.user.db;
+
+public class NavigationDrawerItem {
+    private int iconId;
+    private String title;
+
+    public NavigationDrawerItem(int iconId, String title) {
+        this.iconId = iconId;
+        this.title = title;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
